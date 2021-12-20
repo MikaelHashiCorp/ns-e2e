42fd88c1cd7d4c0c6530dc2f4d321da2b9645837
Mikael Sikora <mikael.sikora@hashicorp.com>
2021-12-20 15:31:44 -0800

    Make 62821-seatgeek relevant

--- terraform/packer/ubuntu-bionic-amd64/setup.sh
+++ terraform/packer/ubuntu-bionic-amd64/setup.sh
@@ -5,9 +5,9 @@
 set -e
 
 # Will be overwritten at test time with the version specified
-NOMADVERSION=1.0.4
-CONSULVERSION=1.9.0
-VAULTVERSION=1.5.4
+NOMADVERSION=1.2.3
+CONSULVERSION=1.11.1
+VAULTVERSION=1.9.1
 
 NOMAD_PLUGIN_DIR=/opt/nomad/plugins/
 
